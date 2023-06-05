639c9155b680b6cb9771aec782a2887472b87e6f
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-06-05 12:48:26 +0200

    Exercism Panagram and Circular Buffer added. README.md files updated.

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/CircularBuffer.cs
@@ -0,0 +1,55 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly Queue<T> _buffer;
+    private readonly int _capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        if (capacity < 1)
+        {
+            throw new ArgumentException("Capacity must be positive.", nameof(capacity));
+        }
+        
+        _buffer = new Queue<T>(capacity);
+        _capacity = capacity;
+    }
+
+    public T Read()
+    {
+        if (_buffer.Count == 0)
+        {
+            throw new InvalidOperationException("Cannot read from an empty buffer.");
+        }
+        
+        return _buffer.Dequeue();
+    }
+
+    public void Write(T value)
+    {
+        if (_buffer.Count == _capacity)
+        {
+            throw new InvalidOperationException("Cannot write to a full buffer.");
+        }
+        
+        _buffer.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (_buffer.Count == _capacity)
+        {
+            _buffer.Dequeue();
+        }
+        
+        _buffer.Enqueue(value);
+    }
+
+    public void Clear()
+    {
+        _buffer.Clear();
+    }
+}
---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/Panagram.cs
@@ -0,0 +1,12 @@
+﻿
+
+public static class Panagram
+{
+    public static bool IsPanagram(string input)
+    {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        string lowerInput = input.ToLower();
+
+        return alphabet.All(letter => lowerInput.Contains(letter));
+    }
+}
