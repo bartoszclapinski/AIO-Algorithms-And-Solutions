5bd967121ea8332fb0e486998221f8c20716ec41
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-06-20 11:32:42 +0200

    Exercism solution in C# added.

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/SpiralMatrix.cs
@@ -0,0 +1,55 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+
+public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        var matrix = new int[size, size];
+
+        var rowStart = 0;
+        var rowEnd = size - 1;
+        var colStart = 0;
+        var colEnd = size - 1;
+
+        var counter = 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd)
+        {
+            // Fill the top row
+            for (var i = colStart; i <= colEnd; i++)
+            {
+                matrix[rowStart, i] = counter++;
+            }
+            rowStart++;
+
+            // Fill the right column
+            for (var i = rowStart; i <= rowEnd; i++)
+            {
+                matrix[i, colEnd] = counter++;
+            }
+            colEnd--;
+
+            // Fill the bottom row
+            if (rowStart <= rowEnd)
+            {
+                for (var i = colEnd; i >= colStart; i--)
+                {
+                    matrix[rowEnd, i] = counter++;
+                }
+                rowEnd--;
+            }
+
+            // Fill the left column
+            if (colStart > colEnd) continue;
+            {
+                for (var i = rowEnd; i >= rowStart; i--)
+                {
+                    matrix[i, colStart] = counter++;
+                }
+                colStart++;
+            }
+        }
+
+        return matrix;
+    }
+}
---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/TheWeatherInDeather.cs
@@ -0,0 +1,91 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+
+using System;
+using System.Collections.Generic;
+public class WeatherStation
+{
+    private Reading _reading;
+    private readonly List<DateTime> _recordDates = new List<DateTime>();
+    private readonly List<decimal> _temperatures = new List<decimal>();
+    public void AcceptReading(Reading reading)
+    {
+        _reading = reading;
+        _recordDates.Add(DateTime.Now);
+        _temperatures.Add(reading.Temperature);
+    }
+    public void ClearAll()
+    {
+        _reading = new Reading();
+        _recordDates.Clear();
+        _temperatures.Clear();
+    }
+    public decimal LatestTemperature => _reading.Temperature;
+    public decimal LatestPressure => _reading.Pressure;
+    public decimal LatestRainfall => _reading.Rainfall;
+    public bool HasHistory => _recordDates.Count > 1;
+    public Outlook ShortTermOutlook
+    {
+        get
+        {
+            if (_reading.Equals(new Reading())) throw new ArgumentException();
+            return _reading is { Pressure: < 10m, Temperature: < 30m } ? Outlook.Cool
+                : _reading.Temperature > 50 ? Outlook.Good
+                : Outlook.Warm;
+        }
+    }
+    public Outlook LongTermOutlook
+    {
+        get
+        {
+            return _reading.WindDirection switch
+            {
+                WindDirection.Southerly => Outlook.Good,
+                WindDirection.Easterly when _reading.Temperature > 20 => Outlook.Good,
+                WindDirection.Northerly => Outlook.Cool,
+                WindDirection.Easterly when _reading.Temperature <= 20 => Outlook.Warm,
+                WindDirection.Westerly => Outlook.Rainy,
+                _ => throw new ArgumentException(),
+            };
+        }
+    }
+    public State RunSelfTest() => _reading.Equals(new Reading()) ? State.Bad : State.Good;
+}
+/*** Please do not modify this struct ***/
+public struct Reading
+{
+    public decimal Temperature { get; }
+    public decimal Pressure { get; }
+    public decimal Rainfall { get; }
+    public WindDirection WindDirection { get; }
+    public Reading(decimal temperature, decimal pressure,
+        decimal rainfall, WindDirection windDirection)
+    {
+        Temperature = temperature;
+        Pressure = pressure;
+        Rainfall = rainfall;
+        WindDirection = windDirection;
+    }
+}
+/*** Please do not modify this enum ***/
+public enum State
+{
+    Good,
+    Bad
+}
+/*** Please do not modify this enum ***/
+public enum Outlook
+{
+    Cool,
+    Rainy,
+    Warm,
+    Good
+}
+/*** Please do not modify this enum ***/
+public enum WindDirection
+{
+    Unknown, // default
+    Northerly,
+    Easterly,
+    Southerly,
+    Westerly
+}
