43dbdabe5a4c13943fc3cab41ec94f5efb855c19
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-07-11 13:07:31 +0200

    Exercism - Football Match Reports solution in Java added

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/Java/FootballMatchReports.java
@@ -0,0 +1,19 @@
+package exercism;
+
+public class FootballMatchReports {
+    public static String onField(int shirtNumber){
+        return switch (shirtNumber) {
+            case 1 -> "goalie";
+            case 2 -> "left back";
+            case 3, 4 -> "center back";
+            case 5 -> "right back";
+            case 6, 7, 8 -> "midfielder";
+            case 9 -> "left wing";
+            case 10 -> "striker";
+            case 11 -> "right wing";
+            default -> throw new IllegalArgumentException("Invalid shirt number: " + shirtNumber);
+        };
+    }
+}
+
+
