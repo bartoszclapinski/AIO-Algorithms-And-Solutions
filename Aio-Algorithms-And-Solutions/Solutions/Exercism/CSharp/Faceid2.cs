19b508bc78f2b90f360f97436089ae2f78d80dab
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-07-20 14:53:30 +0200

    Exercism - Face Id 2.0 solution in C# added.

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/Faceid2.cs
@@ -0,0 +1,73 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+
+using System;
+using System.Collections.Generic;
+
+public class FacialFeatures : IEquatable<FacialFeatures>
+{
+    public string EyeColor { get; }
+    public decimal PhiltrumWidth { get; }
+
+    public FacialFeatures(string eyeColor, decimal philtrumWidth)
+    {
+        EyeColor = eyeColor;
+        PhiltrumWidth = philtrumWidth;
+    }
+
+    public bool Equals(FacialFeatures other)
+    {
+        if (other is null)
+            return false;
+        
+        return EyeColor == other.EyeColor && PhiltrumWidth == other.PhiltrumWidth;
+    }
+
+    public override bool Equals(object obj) => Equals(obj as FacialFeatures);
+    public override int GetHashCode() => HashCode.Combine(EyeColor, PhiltrumWidth);
+}
+
+public class Identity : IEquatable<Identity>
+{
+    public string Email { get; }
+    public FacialFeatures FacialFeatures { get; }
+
+    public Identity(string email, FacialFeatures facialFeatures)
+    {
+        Email = email;
+        FacialFeatures = facialFeatures;
+    }
+
+    public bool Equals(Identity other)
+    {
+        if (other is null)
+            return false;
+
+        return Email == other.Email && FacialFeatures.Equals(other.FacialFeatures);
+    }
+
+    public override bool Equals(object obj) => Equals(obj as Identity);
+    public override int GetHashCode() => HashCode.Combine(Email, FacialFeatures);
+}
+
+public class Authenticator
+{
+    private readonly HashSet<Identity> _identities = new ();
+    private readonly Identity _admin = new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+
+    public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB) => faceA.Equals(faceB);
+
+    public bool IsAdmin(Identity identity) => _admin.Equals(identity);
+
+    public bool Register(Identity identity)
+    {
+        if (_identities.Contains(identity))
+            return false;
+
+        _identities.Add(identity);
+        return true;
+    }
+
+    public bool IsRegistered(Identity identity) => _identities.Contains(identity);
+
+    public static bool AreSameObject(Identity identityA, Identity identityB) => ReferenceEquals(identityA, identityB);
+}
