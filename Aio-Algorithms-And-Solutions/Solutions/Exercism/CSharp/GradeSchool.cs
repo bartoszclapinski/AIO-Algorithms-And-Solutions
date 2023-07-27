5d73702d0cc2a4d8d7b593a9bc53a65820d2bd72
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-07-27 21:48:48 +0200

    Exercism - Grade School solution in C# added

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/GradeSchool.cs
@@ -0,0 +1,43 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GradeSchool
+{
+    private Dictionary<string, int> _roster;
+
+    public GradeSchool()
+    {
+        _roster = new Dictionary<string, int>();
+    }
+    
+    public bool Add(string student, int grade)
+    {
+        if (_roster.ContainsKey(student))
+        {
+            return false; // Indicates that student already exists in the roster
+        }
+        _roster[student] = grade;
+        return true;
+    }
+
+    public IEnumerable<string> Roster()
+    {
+        return _roster
+                        .OrderBy(kv => kv.Value)
+                        .ThenBy(kv => kv.Key)
+                        .Select(kv => kv.Key)
+                        .ToArray();
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        return _roster
+                        .Where(kv => kv.Value == grade)
+                        .OrderBy(kv => kv.Key)
+                        .Select(kv => kv.Key)
+                        .ToArray();
+    }
+}
