f6c53b88bf2d38ea3ce7346bd4f9f97720f6476e
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2023-08-17 12:34:15 +0200

    Exercism - Bob solution in C# added.

---
+++ Aio-Algorithms-And-Solutions/Solutions/Exercism/CSharp/MatchingBrackets.cs
@@ -0,0 +1,19 @@
+﻿namespace Aio_Algorithms_And_Solutions.Solutions.Exercism.CSharp;
+
+using System;
+using System.Collections.Generic;
+public static class MatchingBrackets
+{
+    private static readonly Dictionary<char, char> BracketDict = new () { { '{', '}' }, { '[', ']' }, { '(', ')' }, };
+    public static bool IsPaired(string input)
+    {
+        var bracketStack = new Stack<char>();
+        foreach(var character in input)
+        {
+            if(BracketDict.TryGetValue(character, out var value)) bracketStack.Push(value);
+            else if(BracketDict.ContainsValue(character) && (!bracketStack.TryPop(out char popped) || popped != character))
+                return false;
+        }
+        return bracketStack.Count == 0;
+    }
+}
